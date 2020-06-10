d4cc17c97f13502e9e9ac24bd29cec8e4200aa77
wuzhenzhao <774283325@qq.com>
2020-06-10 20:40:28 +0800

    后台管理模板静态页面初始化

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-
+import './other/css/style.css'
+import './other/javascript/public.js'
+import './other/javascript/plug-ins/pagination.js'
+import $ from 'jquery'
 Vue.prototype.$axios = axios
+Vue.prototype.$ = $
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
